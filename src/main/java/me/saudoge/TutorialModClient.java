3b79cd9aa9b379d504e10d788d079836b8d1480b
SauDoge6597 <arnoldli6597@gmail.com>
2024-12-15 21:50:47 +0800

    Initial commit

---
+++ src/main/java/me/saudoge/TutorialModClient.java
@@ -0,0 +1,10 @@
+package me.saudoge;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
